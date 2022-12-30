a44bd88e32bd823ec510a695831ec4854c641670
davidjm macbook <djmlewis@mac.com>
2022-12-30 15:46:49 +0000

    Initial Commit

---
+++ itinerant Watch App/itinerantApp.swift
@@ -0,0 +1,17 @@
+//
+//  itinerantApp.swift
+//  itinerant Watch App
+//
+//  Created by David JM Lewis on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct itinerant_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ itinerant/itinerantApp.swift
@@ -0,0 +1,17 @@
+//
+//  itinerantApp.swift
+//  itinerant
+//
+//  Created by David JM Lewis on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct itinerantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
